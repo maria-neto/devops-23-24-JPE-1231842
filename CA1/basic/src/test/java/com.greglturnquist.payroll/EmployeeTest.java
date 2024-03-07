7d6a21676b1956563285a320a3c243508ea5e255
Maria Neto <1231842@isep.ipp.pt>
2024-03-07 14:22:53 +0000

    import and download of libraries

--- CA1/basic/src/test/java/com.greglturnquist.payroll/EmployeeTest.java
+++ CA1/basic/src/test/java/com.greglturnquist.payroll/EmployeeTest.java
@@ -1,2 +1,19 @@
-package com.greglturnquist.payroll;public class EmployeeTest {
+package com.greglturnquist.payroll;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+
+public class EmployeeTest {
+
+    @Test
+    public void createEmployee(){
+        String firstName = "Frodo";
+        String lastName = "Baggins";
+        String description = "ring bearer";
+        int jobYears = 4;
+        Employee employee = new Employee(firstName, lastName, description, jobYears);
+        assertNotNull(employee);
+
+    }
 }
